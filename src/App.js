73d8239b93cafb3e91f26157773ef41d3ec11dfa
Kirk Besa <kirkpatrickbesa@gmail.com>
2024-09-18 02:01:34 +0800

    finished

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Counter from './components/Counter.js';
 import Event from './components/Event.js';
 import Form from './components/Form.js';
+import Navigation from './pages/Navigation.js';
+import Home from './pages/Home.js';
+import About from './pages/About.js';
+import Contact from './pages/Contact.js';
+import NotFound from './pages/NotFound.js';
+
 
 function App() {
   return (
-    <div className="App">
-      {/* <Counter /> */}
-      {/* <Event /> */}
-      {/* <Form /> */}
+    <Router>
+      <div className="App">
+        <Navigation />
 
-      
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route exact path="/counter" element={<Counter />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
 
-    </div>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div>
+            <p>You are in: About Us</p>
+        </div>
+    );
+}
